88ca0a0244d33d3d4bf80aa040bdade03b7d0ffb
AlexSSI <alex.sajama@salamancasolutions.com>
2023-05-12 19:47:14 -0400

    login styles add

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,16 +1,114 @@
-import { View, Text,Button } from 'react-native'
+import { View, Text, Image, StyleSheet, ScrollView, TouchableOpacity, Button } from 'react-native'
 import React from 'react'
+
+const menuItems = [
+    {
+        id: 1,
+        title: 'Hamburguesa',
+        description: 'Deliciosa hamburguesa con queso, tocino y papas fritas',
+        price: 'Bs. 9.99',
+        image: require('../assets/descarga.jpeg'),
+    },
+    {
+        id: 2,
+        title: 'Ensalada César',
+        description: 'Crujiente lechuga, pollo a la parrilla, queso parmesano y aderezo César',
+        price: 'Bs. 7.99',
+        image: require('../assets/2.jpeg'),
+    },
+    {
+        id: 3,
+        title: 'Pizza Margherita',
+        description: 'Pizza clásica con tomate, mozzarella fresca y albahaca',
+        price: 'Bs. 12.99',
+        image: require('../assets/3.jpeg'),
+    },
+];
 
 export default function HomeScreen(props) {
     const { navigation } = props;
+    console.log(props);
 
     const goToPage = (pageName) => {
         navigation.navigate(pageName);
     }
     return (
-        <View>
-            <Text>homeScreen</Text>
-            <Button onPress={() => goToPage("Profile")} title="Go to Profile" ></Button>
-        </View>
+        <ScrollView style={styles.container}>
+            <View style={styles.header}>
+                <Text style={styles.title}>Bienvenido a nuestro restaurante</Text>
+            </View>
+            <View style={styles.menu}>
+                {menuItems.map((item) => (
+                    <TouchableOpacity
+                        key={item.id}
+                        style={styles.menuItem}
+                        onPress={() => handleMenuItemPress(item.id)}
+                    >
+                        <Image style={styles.menuItemImage} source={item.image} />
+                        <View style={styles.menuItemInfo}>
+                            <Text style={styles.menuItemTitle}>{item.title}</Text>
+                            <Text style={styles.menuItemDescription}>{item.description}</Text>
+                            <Text style={styles.menuItemPrice}>{item.price}</Text>
+                        </View>
+                    </TouchableOpacity>
+                ))}
+            </View>
+            {/*<Button onPress={() => goToPage("Profile")} title="Go to Profile" ></Button>*/}
+        </ScrollView>
     )
 }
+
+
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#f2f2f2',
+    },
+    header: {
+        backgroundColor: '#0077cc',
+        paddingVertical: 20,
+        paddingHorizontal: 30,
+    },
+    title: {
+        fontSize: 24,
+        color: '#fff',
+    },
+    menu: {
+        flexWrap: 'wrap',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        paddingHorizontal: 20,
+        paddingVertical: 10,
+    },
+    menuItem: {
+        width: '48%',
+        backgroundColor: '#fff',
+        borderRadius: 5,
+        marginBottom: 20,
+        padding: 10,
+    },
+    menuItemImage: {
+        height: 120,
+        borderRadius: 5,
+        marginBottom: 10,
+    },
+    menuItemInfo: {
+        alignItems: 'center',
+    },
+    menuItemTitle: {
+        fontSize: 18,
+        fontWeight: 'bold',
+        marginBottom: 5,
+    },
+    menuItemDescription: {
+        fontSize: 14,
+        marginBottom: 5,
+        textAlign: 'center',
+    },
+    menuItemPrice: {
+        fontSize: 16,
+        fontWeight: 'bold',
+        color: '#0077cc',
+    },
+});
--- src/screens/SimpleSignInScreen.js
+++ src/screens/SimpleSignInScreen.js
@@ -1,32 +1,107 @@
-import { View, Text, TextInput, Button, StyleSheet } from 'react-native'
-import React from 'react'
+import { View, Text, TextInput, TouchableOpacity, StyleSheet } from 'react-native'
+import React, { useState } from 'react';
 
 const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#7a1010',
+    },
+    title: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        marginBottom: 20,
+        color: '#FFF'
+    },
+    error: {
+        color: 'red',
+        marginBottom: 10,
+        color: '#FFF'
+    },
+    formGroup: {
+        marginBottom: 10,
+        width: '80%',
+    },
+    label: {
+        fontSize: 16,
+        marginBottom: 5,
+        color: 'yellow'
+    },
     input: {
         height: 40,
-        margin: 12,
-        borderWidth: 1,
         padding: 10,
+        borderRadius: 5,
+        borderWidth: 1,
+        borderColor: '#ccc',
+        fontSize: 16,
+        color: '#F7C73E'
+    },
+    button: {
+        backgroundColor: '#527A10',
+        borderRadius: 5,
+        padding: 10,
+        width: '80%',
+    },
+    buttonText: {
+        color: '#fff',
+        fontSize: 16,
+        fontWeight: 'bold',
+        textAlign: 'center',
     },
 });
 
 export default function SimpleSignInScreen(props) {
     const { navigation, route } = props;
-    const [email, setEmail] = React.useState('');
-    const [password, setPassword] = React.useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [errorMessage, setErrorMessage] = useState('');
     const { setUserToken } = route.params;
+    const handleEmailChange = (text) => {
+        setEmail(text);
+    };
+
+    const handlePasswordChange = (text) => {
+        setPassword(text);
+    };
+
+    const handleSubmit = () => {
+        // Validación del formulario
+        if (!email.includes('@') || password.length < 6) {
+            setErrorMessage('Email o contraseña inválidos');
+        } else {
+            // Envío del formulario
+            console.log('Email:', email);
+            console.log('Contraseña:', password);
+            setUserToken('Token');
+            // ... código para enviar la información del formulario al servidor ...
+        }
+    };
     return (
-        <View>
-            <Text>Email</Text>
-            <TextInput style={styles.input} onChangeText={setEmail} />
-            <Text>Password</Text>
-            <TextInput
-                style={styles.input}
-                onChangeText={setPassword}
-                placeholder="Password"
-                secureTextEntry={true}
-            />
-            <Button title="Sign Up" onPress={() => setUserToken('token')} />
+        <View style={styles.container}>
+            <Text style={styles.title}>Login</Text>
+            {errorMessage && <Text style={styles.error}>{errorMessage}</Text>}
+            <View style={styles.formGroup}>
+                <Text style={styles.label}>Email:</Text>
+                <TextInput
+                    style={styles.input}
+                    keyboardType="email-address"
+                    value={email}
+                    onChangeText={handleEmailChange}
+                />
+            </View>
+            <View style={styles.formGroup}>
+                <Text style={styles.label}>Contraseña:</Text>
+                <TextInput
+                    style={styles.input}
+                    secureTextEntry={true}
+                    value={password}
+                    onChangeText={handlePasswordChange}
+                />
+            </View>
+            <TouchableOpacity style={styles.button} onPress={handleSubmit}>
+                <Text style={styles.buttonText}>Iniciar sesión</Text>
+            </TouchableOpacity>
         </View>
     )
 }
