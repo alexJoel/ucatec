ec0aece05aba88a6a8ac49521c603cedadb00a8a
AlexSSI <alex.sajama@salamancasolutions.com>
2023-05-14 20:51:29 -0400

    delete

--- carpetaT/src/screens/FavoriteScreen.js
+++
@@ -1,9 +0,0 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-export default function FavoriteScreen() {
-  return (
-    <View>
-      <Text>FavoriteScreen</Text>
-    </View>
-  )
-}
